f0809c1a8f319be9c992d7a5410fdc81a8a9b6f6
Maria <mariya.syrokvash@gmail.com>
2024-10-26 23:40:32 +0200

    fix build

--- src/db/db.ts
+++ src/db/db.ts
@@ -9,3 +9,24 @@ export type ReadonlyDBType = {
     blogs: Readonly<BlogDbType[]>
     posts: Readonly<PostDbType[]>
 }
+
+
+
+
+export const db: DBType = {
+    blogs: [],
+    posts: [],
+}
+
+export const setDB = (dataset?: Partial<ReadonlyDBType>) => {
+    if (!dataset) {
+        db.blogs = []
+        db.posts = []
+        return
+    }
+
+    // если что-то передано - то заменяем старые значения новыми,
+    // не ссылки - а глубокое копирование, чтобы не изменять dataset
+    db.blogs = dataset.blogs?.map(b => ({...b})) || db.blogs
+    db.posts = dataset.posts?.map(p => ({...p})) || db.posts
+}
